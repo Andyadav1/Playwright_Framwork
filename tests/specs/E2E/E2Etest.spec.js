bcd811329718070bace3b93e48b71249c0851a8d
Andyadav1 <anandkhushi309@gmail.com>
2026-03-09 21:06:42 +0530

    E2E changes

--- tests/specs/E2E/E2Etest.spec.js
+++ tests/specs/E2E/E2Etest.spec.js
@@ -1,5 +1,5 @@
 import test from "@playwright/test";
-
+import test_data from "./test_data/"
 test.describe(`Verify that the list of accounts on the Accounts are Sortable`, () => {
-    test.beforeAll(`login as `)
+    test.beforeAll(`login as  `)
 });
